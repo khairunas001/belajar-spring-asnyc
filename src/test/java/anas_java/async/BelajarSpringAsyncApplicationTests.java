51b653cda1d00da6f03db09071957e7469830631
khairunas001 <khairunas2020@mail.ugm.ac.id>
2026-01-13 16:56:13 +0700

    first commit

---
+++ src/test/java/anas_java/async/BelajarSpringAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package anas_java.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
